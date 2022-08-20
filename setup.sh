f3024b0c2bb7f9fc0650376b57b5aecd4eba8267
majjejjam <miro.allard@pm.me>
2022-08-20 18:24:20 +0200

    added package installation, firefox theme and keyd conf

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Install packages
+echo "Installing paru and previous packages"
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ..
+sudo pacman -S --needed - < pkglist.txt
+
+# Keyd
+echo "Adding configs"
+cp default.conf /etc/keyd/default.conf
+
+# Firefox Gnome Theme
+echo "Adding firefox gnome theme"
+git clone https://github.com/rafaelmardojai/firefox-gnome-theme && cd firefox-gnome-theme
+git checkout beta
+./scripts/auto-install.sh
+cd ..
+
+echo "Add ILoveCandy, uncomment Color and ParalellDownloads = 5 to /etc/pacman.conf"
