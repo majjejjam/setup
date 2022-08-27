744eb6180d8fd92bc64fe6ec3161b02eb23551a6
majjejjam <miro.allard@pm.me>
2022-08-27 20:07:34 +0200

    added flatpaks

--- setup.sh
+++ setup.sh
@@ -34,3 +34,6 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 git clone --bare git@github.com:HappyCerberus/dotfiles-demo.git $HOME/.dotfiles
 dotfiles config --local status.showUntrackedFiles no
 dotfiles checkout
+
+# Install flatpaks
+xargs flatpak install -y < flatpaks.txt
