d571d847920b0d03d221fe65d704dc72bcc8f9d3
majjejjam <miro.allard@pm.me>
2022-08-28 19:10:10 +0200

    added dark-headers and clean up

--- setup.sh
+++ setup.sh
@@ -1,39 +1,46 @@
 #!/bin/sh
 
-# Install packages
-echo "Installing paru and previous packages"
+# Install pacman packages
+echo 'Installing paru and previous packages'
 sudo pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
+git clone https://aur.archlinux.org/paru.git && cd paru
 makepkg -si
 cd ..
 sudo pacman -S --needed - < pkglist.txt
 
-# Keyd
-echo "Adding configs"
-cp default.conf /etc/keyd/default.conf
+# Install flatpaks
+echo 'Installing flatpaks'
+xargs flatpak install -y < flatpaks.txt
 
 # Firefox Gnome Theme
-echo "Adding firefox gnome theme"
+echo 'Adding firefox gnome theme'
 git clone https://github.com/rafaelmardojai/firefox-gnome-theme && cd firefox-gnome-theme
-git checkout beta
 ./scripts/auto-install.sh
 cd ..
 
-echo "Add ILoveCandy, uncomment Color and ParalellDownloads = 5 to /etc/pacman.conf"
+echo 'Add yourself ILoveCandy, uncomment Color and ParalellDownloads = 5 to /etc/pacman.conf'
 
 # Change shell to zsh
-echo "Changing shell to zsh"
+echo 'Changing shell to zsh'
 chsh -s /bin/zsh
 
 # Load dconf settings
 dconf load / < saved_settings.dconf
 
-# Add dotfiles and dotfiles git repo
+# configs
+echo 'Adding configs'
+echo 'dotfiles…'
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 git clone --bare git@github.com:majjejjam/dotfiles.git $HOME/.dotfiles
 dotfiles config --local status.showUntrackedFiles no
 dotfiles checkout
 
-# Install flatpaks
-xargs flatpak install -y < flatpaks.txt
+echo 'keyd…'
+cp default.conf /etc/keyd/default.conf
+
+echo 'dark=headers…'
+git clone https://github.com/catvitalio/dark-headers && cd dark-headers
+chmod +x install.sh
+./install.sh
+systemctl --user enable --now dark-headers.service
+cd ..
